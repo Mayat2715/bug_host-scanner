06f68ee5be758691ddbcf4cdcdbac86695bfa73e
MrStalker121 <41187956+MrStalker121@users.noreply.github.com>
2018-11-18 17:35:24 +0700

    Update touchme.sh

--- touchme.sh
+++ touchme.sh
@@ -1,5 +1,6 @@
 pkg install -y nmap curl wget
 a='output'
 mkdir $a $a/nmap $a/wget $a/curl
+clear
 echo '\033[0m[\033[1;31m!\033[0m]\033[1;33mselesai!'
 echo '\033[0m[\033[1;31m!\033[0m]\033[1;33mbisa memulai scanner dengan perintah python2 scanner.py'
